a88f7b4039b84e97b8d84e465e68df58b5f36f02
EC2 Default User <ec2-user@ip-172-31-54-176.ec2.internal>
2021-10-09 19:58:18 +0000

    first commit

--- app/Http/Controllers/HotelsController.php
+++ app/Http/Controllers/HotelsController.php
@@ -29,7 +29,7 @@ class HotelsController extends Controller
         return view('hotels')
             ->with([
                 'hotels' => $hotels,
-                // 'isliked' => $isliked
+               cd cms
             ]);
             
        
